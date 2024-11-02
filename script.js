ad638a2b9a02130449dac7a0bbda209fe3f1437c
Student <Student@gmmg.com>
2024-11-02 17:00:19 +0200

    all done

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  fetch(
+    "https://raw.githubusercontent.com/MamboDancer/WebMiddleCyberween/main/task.json"
+  )
+    .then((response) => response.json())
+    .then((data) => show(data));
+
+  function show(data) {
+    console.log(data);
+    console.log(data.event);
+    console.log(data.event);
+    data.guests.forEach((e) => {
+      $(".content").append(`
+          <div class='page'>
+          <h1>${e.name}</h1>
+           <p>Description: ${e.about}</p>
+           <img src="${e.photo_url}" alt="Photo about ${e.name}" width="300px">
+           </div>`);
+    });
+  }
+});
